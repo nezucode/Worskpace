06043e20e3af4fcf9745b3d390b90cc08c2a8e67
nezucode <intann89.in@gmail.com>
2023-12-10 17:09:35 +0700

    main

---
+++ src/load.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
+import java.util.InputMismatchException;  
+
+public class load {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
